c9189481e0b81d717690662427396170505ae038
degoeym <degoeym@gmail.com>
2016-03-12 13:17:51 -0500

    Added items for workouts

--- drift/db/schema.rb
+++ drift/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311004722) do
+ActiveRecord::Schema.define(version: 20160312180537) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
     t.decimal  "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
