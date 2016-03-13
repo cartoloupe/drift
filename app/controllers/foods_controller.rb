ed0d1705acef7fbf9338a01c002e30cd51043d0d
cartoloupe <vasanth.pappu@gmail.com>
2016-03-13 13:04:37 -0400

    add crud links to foods index

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,11 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: [:edit, :update, :destroy]
+
   def index
     @food = Food.all
 
-    @total = @food.all.where(["created_at > ?", "2016-01-01 00:00:00"]).map{ |f| f.cost }.reduce(&:+)
+    @total = Food.all.last_thirty.map{ |f| f.cost }.reduce(&:+)
+    @last_week = Food.all.last_week.map{ |f| f.cost }.reduce(&:+)
   end
 
   def show
@@ -13,14 +16,43 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @food.update(food_params)
+        format.html { redirect_to foods_path, notice: 'Food was successfully updated.' }
+        format.json { render :show, status: :ok, location: @food }
+      else
+        format.html { render :edit }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def create
-    #binding.pry
     @food = Food.new(food_params)
-    @food.save
-    redirect_to @food
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @food.destroy
+    respond_to do |format|
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_food
+      @food = Food.find(params[:id])
+    end
 
   def food_params
     params.require(:food).permit(:name, :cost)
