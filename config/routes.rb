fbc63a991420982429d2d104eb7b66375bdbe633
cartoloupe <vasanth.pappu@gmail.com>
2016-03-13 11:56:08 -0400

    remove welcome route; set root route to foods#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   resources :workouts
-  get 'foods/show'
-
-  get 'foods/new'
-
-  get 'foods/create'
+  resources :foods
+  root 'foods#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  resources :foods
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
