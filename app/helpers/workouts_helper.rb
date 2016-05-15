fa2d914ec941178e1508fc375179c86c835e8ab9
cartoloupe <vasanth.pappu@gmail.com>
2016-05-15 12:36:54 -0400

    date selection

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,5 +1,11 @@
 module WorkoutsHelper
-  def push_press
-    Workouts.all.push_press
+  def last_ten_days
+    10.times.map do |n|
+      t = (Time.now - n.days)
+      [
+        t.strftime('%Y-%m-%d %a'),
+        t.strftime('%Y-%m-%d')
+      ]
+    end
   end
 end
